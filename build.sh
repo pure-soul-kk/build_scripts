ba6da49b828e333a0673b67147f55e4fecbf3004
K R I S H N A <krishnakripa34567@gmail.com>
2024-06-01 01:14:59 +0530

    build_script: adapt for PixelOS

--- build.sh
+++ build.sh
@@ -9,21 +9,35 @@ git clone https://github.com/pure-soul-kk/local_manifest -b pixelos .repo/local_
 if [ -f /opt/crave/resync.sh ]; then
   /opt/crave/resync.sh
 else
-  repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
+  repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 fi
 
 #custom repos
-rm -rf prebuilts/clang/host/linux-x86/clang-r498229b
-git clone --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r498229b.git -b 13.0 prebuilts/clang/host/linux-x86/clang-r498229b
-
+rm -rf prebuilts/clang/host/linux-x86/clang-r530567
+git clone --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r530567.git -b 14.0 prebuilts/clang/host/linux-x86/clang-r530567
+rm -rf hardware/xiaomi
 git clone https://github.com/pure-soul-kk/hardware_xiaomi hardware/xiaomi
+rm -rf vendor/extra
 git clone https://github.com/pure-soul-kk/vendor_extra vendor/extra
+rm -rf frameworks/base
+git clone --depth=1 https://github.com/AOSP-Sweet-Trees/android_frameworks_base frameworks/base
+rm -rf frameworks/av
+git clone --depth=1 https://github.com/AOSP-Sweet-Trees/android_frameworks_av frameworks/av
+rm -rf packages/apps/Settings
+git clone --depth=1 https://github.com/AOSP-Sweet-Trees/android_packages_apps_Settings packages/apps/Settings
+rm -rf frameworks/opt/telephony
+git clone --depth=1 https://github.com/AOSP-Sweet-Trees/frameworks_opt_telephony frameworks/opt/telephony
+rm -rf vendor/aosp
+git clone --depth=1 https://github.com/AOSP-Sweet-Trees/vendor_aosp vendor/aosp
+rm -rf vendor/aosp/signing/keys
+git clone --depth=1 https://github.com/pure-soul-kk/private-keys vendor/aosp/signing/keys
+rm -rf packages/modules/Bluetooth
+git clone --depth=1 https://github.com/AOSP-Sweet-Trees/packages_modules_Bluetooth packages/modules/Bluetooth
+rm -rf packages/modules/Wifi
+git clone --depth=1 https://github.com/AOSP-Sweet-Trees/android_packages_modules_Wifi packages/modules/Wifi
 
 #build
 . build/envsetup.sh
-lunch aosp_sweet-ap1a-user
-make installclean
-m target-files-package otatools
-
-#sign build
-rm -rf sign.sh && wget https://raw.githubusercontent.com/pure-soul-kk/build_scripts/pos/sign.sh && bash sign.sh
+lunch aosp_sweet-ap2a-user
+mka installclean
+mka bacon
